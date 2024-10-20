3825e477c1af6effecfbcd1cd431c74a30d0ebb4
Ssteynbreher <ssteynbreher@mail.ru>
2024-10-20 20:06:52 +0700

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-a=int(input())
-b=int(input())
-c=int(input())
-if a==b==c:
-    print('Треугольник равносторонний')
-if a==b or a==c or b==c:
-    print('Треугольник равнобедренный')
-else:
-    print('Треугольник разносторонний')
