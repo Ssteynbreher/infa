5ca78868b6b8f38e7a6a1d7b5d5ee3985fd5b252
Ssteynbreher <ssteynbreher@mail.ru>
2024-10-21 09:08:20 +0700

    Update 3.py

--- lab6/3.py
+++ lab6/3.py
@@ -1,8 +1,8 @@
-def calculate_sum(n):
+def f(n):
  sum = 0
  for i in range(1, n + 1):
   sum += i * i
  return sum
 n = int(input("Введите n: "))
-result = calculate_sum(n)
-print(result)
+res = f(n)
+print(res)
