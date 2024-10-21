f683c16350e1ab24730c43f7e09f73d7b137fde9
Ssteynbreher <ssteynbreher@mail.ru>
2024-10-21 09:10:33 +0700

    Update 5.py

--- lab6/5.py
+++ lab6/5.py
@@ -1,15 +1,11 @@
-def dog_age_to_human(dog_age):
-  if dog_age < 0:
+def f(n):
+  if n < 0:
     return "Ошибка!"
-  elif dog_age <= 2:
-    return dog_age * 10.5
+  elif n <= 2:
+    return n * 10.5
   else:
-    return 21 + (dog_age - 2) * 4
-
-# Получение собачьего возраста от пользователя
-dog_age = int(input("Введите собачий возраст: "))
-
-# Вычисление и вывод человеческого возраста
-human_age = dog_age_to_human(dog_age)
-print("Введенный вами год эквивалентен", human_age, "человеческим")
+    return 21 + (n - 2) * 4
+n = int(input("Введите возраст: "))
+h = f(n)
+print("Введенный вами год эквивалентен", h, "человеческим")
 
